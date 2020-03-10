d5026770a1983ed6f8128bf4ebe0227b3e7ae342
Your Name <you@example.com>
2020-03-10 17:09:01 +0700

    Thirth Commit

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -31,7 +30,6 @@ app.use(function(req, res, next) {
 
 app.use('/', appRoutes);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.render('index');
 });
